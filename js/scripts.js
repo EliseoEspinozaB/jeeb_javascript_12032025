1e7b80f1bdd44cc9edfbf866709a9c57127a7a4a
EliseoEspinozaB <ia.eliseoespinoza@ufg.edu.sv>
2025-03-13 01:49:56 -0600

    solucionado

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,20 @@ function crearElemento(url = null){
         url = url
     }
     else{
-        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_f_109008395_OQDupHMza1V6CNOzrJwWAKlakT4IsRW.jpg"
+        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_F_109008395_OQDupHMza1V6CNOzrJwWAKlakT4IsRW.jpg"
     }
+
+    let objetoimagen = document.createElement("img");
+objetoimagen.id = "imagencreada";
+objetoimagen.src = url;
+objetoimagen.alt = "Icono sobre control de videojuegos";
+objetoimagen.tilte = "CONTROL DE VIDEOJUEGO";
+objetoimagen.width = 220;
+objetoimagen.height = 165;
+objetoimagen.addEventListener('click',function(){
+    objetoimagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkU5595AC-bbsoPF-Ik0WzUbVj9rnAbs9lkw&s";
+    objetoimagen.width = 225;
+    objetoimagen.height = 225;
+});
+document.getElementById('lienzo').appendChild(objetoimagen)
 }
